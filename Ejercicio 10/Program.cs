3e79e803455be3295771d0b0ffcfb9888d84fecd
Deivi13 <61521077+Deivi13@users.noreply.github.com>
2021-03-05 00:58:11 -0400

    Add files via upload

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personas persona = new Personas();
+
+            persona.Cargador();
+
+            Console.Read();
+        }
+    }
+
+    class Personas
+    {
+        public void Cargador()
+        {
+            Console.WriteLine("Escriba un nombre: ");
+            string Nombre = Console.ReadLine();
+            Console.WriteLine("Escriba una edad: ");
+            int Edad = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Usted introdujo lo siguiente: ");
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Edad: " + Edad);
+
+            if (Edad >= 18 )
+            {
+                Console.WriteLine("Usted es mayor de edad, tiene " + Edad);
+            }
+        }
+    }
+}
---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            klk tabla = new klk();
+
+            tabla.multiplicador();
+
+            Console.Read();
+        }
+    }
+
+    class klk
+    {
+
+        public void multiplicador()
+        {
+            int valor = 0;
+
+            while(true){
+
+                Console.WriteLine("Introduzca un valor: ");
+                valor = int.Parse(Console.ReadLine());
+
+                if (valor == -1) { Environment.Exit(0); }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("La multiplcacion entre {1} por {0} es: {2} ", i, valor, (i * valor));
+                }
+            }
+                                                                     
+        }
+        
+        
+    }
+}
